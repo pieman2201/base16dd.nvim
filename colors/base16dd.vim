34701941c1c882cdef0d519b28ce8cf03939b0c5
Ishan Kamat <ishankamat123@gmail.com>
2023-04-26 22:19:18 -0500

    rename

---
+++ colors/base16dd.vim
@@ -0,0 +1,4 @@
+lua << EOF
+local base16dd = require("base16dd")
+base16dd.setup({})
+EOF
---
+++ lua/base16dd/init.lua
@@ -0,0 +1,18 @@
+local M = {}
+local theme = require('base16dd.theme')
+
+M.setup = function()
+  vim.cmd('hi clear')
+
+  vim.o.background = 'dark'
+  if vim.fn.exists('syntax_on') then
+    vim.cmd('syntax reset')
+  end
+
+  vim.o.termguicolors = true
+  vim.g.colors_name = 'base16dd'
+
+  theme.set_highlights()
+end
+
+return M
